9436a4339e17a8f1a34c04e1e251dfecf52e6af1
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-12 09:31:43 +0100

    Fix : Correctif de la class Responcer

--- src/module/Responcer.js
+++ src/module/Responcer.js
@@ -6,27 +6,41 @@
  * @data        => undefined
  */
 class Responcer {
-    constructor(conf = {}) {
-        this.status = conf.status       ||  500
-        this.message = conf.message     ||  "Erreur serveur"
-        this.data = conf.status         ||  undefined
+    constructor(req ,res, conf = {}) {
+        this.req = req ;
+        this.res = res ;
+        this.status = conf.status       ||  500 ;
+        this.message = conf.message     ||  "Erreur serveur" ;
+        this.data = conf.status         ||  undefined ;
+        this.isSend = false
     }
 
     statusStr(){
         return (200 <= this.status && this.status <300) ? "SUCCESS" : "FAIL"
     }
 
-    send(response){
-        return response.status(this.status).json({
-            status: this.statusStr(),
-            message: this.message,
-            data: this.data
-        })
+    info(){
+        return `${this.req.method} | ${this.req.originalUrl} | ${this.status} | ${this.statusStr()}\n${this.message}`
     }
 
-    info(){
-        return `${this.status} | ${this.statusStr()}\n${this.message}`
+    send()
+    {
+        if (! this.isSend)
+        {
+            this.isSend = true ;
+
+            console.log(this.info()) ;
+
+            this.res
+                .status(this.status)
+                .json({
+                    status: this.statusStr(),
+                    message: this.message,
+                    data: this.data
+                }) ;
+        }
     }
+
 }
 
 module.exports = Responcer
