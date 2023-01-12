2c4173b53ef25b9d01db39fa5b55c5f1df322444
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-12 09:37:45 +0100

    Fix : Adaptation des routes pour la nouvelle Class responcer

--- src/routes/ticketsRouter.js
+++ src/routes/ticketsRouter.js
@@ -8,7 +8,7 @@ const ticketsRouter = express.Router() ;
 
 ticketsRouter.get('/', async (req, res) => 
 {
-    let rpcr = new Responcer()
+    let rpcr = new Responcer(req, res) ;
     
     try 
     {
@@ -17,19 +17,19 @@ ticketsRouter.get('/', async (req, res) =>
         rpcr.status = 200 ;
         rpcr.message = `Récupération de ${data.rowCount} tickets` ;
         rpcr.data = data.rows ;
+        rpcr.send() ;
     }
     catch (err) 
     {
-        console.log(err.stack)
+        console.log(err.stack);
+        rpcr.send() ;
     }
-    console.log(`get | /api/tickets | ${rpcr.info()}`);
-    res = rpcr.send(res)
 })
 
 
 ticketsRouter.get('/:id', async (req, res) => 
 {
-    let rpcr = new Responcer()
+    let rpcr = new Responcer(req, res) ;
     const id = req.params.id
     
     // Vérifiction du Type de l'id entrante
@@ -37,164 +37,134 @@ ticketsRouter.get('/:id', async (req, res) =>
     {
         rpcr.status = 400 ;
         rpcr.message = `${id} n'est pas un nombre entier` ;
+        rpcr.send() ;
     } 
-    else 
-    {
-        try 
-        {
-            const data = await client.query('SELECT * FROM tickets WHERE id=$1', [id]);
-    
-            // Vérifiction de l'existence de l'id
-            if (data.rows.length === 1) {
-                
-                rpcr.status = 200 ;
-                rpcr.message = `Récupération du ticket ${id}` ;
-                rpcr.data = data.rows[0] ;
 
-            } else {
-                rpcr.status = 404 ;
-                rpcr.message = `Le ticket ${id} n'existe pas` ;
-            }
-        }
-        catch (err) 
+    try 
+    {
+        const data = await client.query('SELECT * FROM tickets WHERE id=$1', [id]);
+
+        // Vérifiction de l'existence de l'id
+        if (data.rows.length === 1) {
+            
+            rpcr.status = 200 ;
+            rpcr.message = `Récupération du ticket ${id}` ;
+            rpcr.data = data.rows[0] ;
+            rpcr.send() ;
+
+        } 
+        else 
         {
-            console.log(err.stack)
+            rpcr.status = 404 ;
+            rpcr.message = `Le ticket ${id} n'existe pas` ;
+            rpcr.send() ;
         }
     }
-    console.log(`get | /api/tickets/${id} | ${rpcr.info()}`);
-    res = rpcr.send(res)
+    catch (err) 
+    {
+        console.log(err.stack)
+        rpcr.send() ;
+    }
 })
 
 ticketsRouter.post('/', async (req, res) => 
 {
-    let rpcr = new Responcer()
+    let rpcr = new Responcer(req, res) ;
     const { user_id, message} = req.body;
 
     // Vérifiction du Type du user_id entrant
-    if (faillingId(user_id))
+    if (faillingId(user_id) || faillingString(message))
     {
         rpcr.status = 400 ;
-        rpcr.message = `${user_id} n'est pas un nombre entier` ;
+        rpcr.message = `Structure incorrect : { message : string , user_id : number }` ;
+        rpcr.send() ;
     } 
-    else if (faillingString(message))
+    
+    try 
     {
-        rpcr.status = 400 ;
-        rpcr.message = `${message} n'est pas un texte` ;
-    } 
-    else 
-    {
-        try 
-        {
-            // Vérifiction de la presence des paramètres nécessaires
-            if (message !== undefined  && user_id !== undefined ) 
-            {
-                const usersList = await client.query('SELECT id FROM users WHERE id = $1',[user_id]);
+        const usersList = await client.query('SELECT id FROM users WHERE id = $1',[user_id]);
 
-                // Vérifiction de l'existence du user_id
-                if (usersList.rowCount === 1)
-                {
-                    const data = await client.query('INSERT INTO tickets (message, user_id) VALUES ($1,$2) RETURNING *', [message, user_id]);
-
-                    rpcr.status = 201 ;
-                    rpcr.message = `Création du ticket ${data.rows[0].id}` ;
-                    rpcr.data = data.rows[0] ;
-                } 
-                else 
-                {
-                    rpcr.status = 404 ;
-                    rpcr.message = `L'utilisateur ${user_id} n'existe pas` ;
-                }
-            } 
-            else 
-            {
-                rpcr.status = 400 ;
-                rpcr.message = "Structure incorrect : { message : string , user_id : number }" ;
-            }
-        }
-        catch (err) 
+        // Vérifiction de l'existence du user_id
+        if (usersList.rowCount === 1)
         {
-            console.log(err.stack)
+            const data = await client.query('INSERT INTO tickets (message, user_id) VALUES ($1,$2) RETURNING *', [message, user_id]);
+
+            rpcr.status = 201 ;
+            rpcr.message = `Création du ticket ${data.rows[0].id}` ;
+            rpcr.data = data.rows[0] ;
+            rpcr.send() ;
+        } 
+        else 
+        {
+            rpcr.status = 404 ;
+            rpcr.message = `L'utilisateur ${user_id} n'existe pas` ;
+            rpcr.send() ;
         }
     }
-    console.log(`post | /api/tickets | ${rpcr.info()}`);
-    res = rpcr.send(res)
+    catch (err) 
+    {
+        console.log(err.stack)
+        rpcr.send() ;
+    }
 })
 
 ticketsRouter.put('/', async (req, res) => 
 {
-    let rpcr = new Responcer()
+    let rpcr = new Responcer(req, res) ;
     const { id, message, done} = req.body;
 
     // Vérifiction de la presence des paramètres nécessaires
-    if (!(id !== undefined && (message !== undefined || done !== undefined))) 
+    if (faillingId(id) || (faillingString(message) && faillingBool(done))) 
     {
         rpcr.status = 400 ;
         rpcr.message = "Structure incorrect : { id : number , message : string , done : boolean } ou { id : number , message : string } ou  { id : number , done : boolean }" ;
-    }
-    // Vérifiction du Type de l'id entrante
-    else if (faillingId(id))
-    {
-        rpcr.status = 400 ;
-        rpcr.message = `${id} n'est pas un nombre entier` ;
-    }
-    // Vérifiction du Type du message entrant
-    else if (faillingString(message))
-    {
-        rpcr.status = 400 ;
-        rpcr.message = `${message} n'est pas un texte` ;
+        rpcr.send() ;
     }
 
-    // Vérifiction du Type du done entrant
-    else if (faillingBool(done))
+    try 
     {
-        rpcr.status = 400 ;
-        rpcr.message = `${done} n'est pas un booleen` ;
-    }
-    else {
+        let data
+        
+        // Exécution de la bonne requête en fonction des paramètres
+        console.log(faillingString(message) , faillingBool(done));
+        if ( ! (faillingString(message) || faillingBool(done))) 
+        {
+            data = await client.query('UPDATE tickets SET message = $1, done = $2 WHERE id = $3 RETURNING *', [message, done, id]);
+        } 
+        else if (! faillingString(message))
+        {
+            data = await client.query('UPDATE tickets SET message = $1 WHERE id = $2 RETURNING *', [message, id]);
+        } 
+        else 
+        {
+            data = await client.query('UPDATE tickets SET done = $1 WHERE id = $2 RETURNING *', [done, id]);
+        }
 
-        try 
+        // Vérifiction de l'existence de l'id
+        if (data.rowCount > 0) 
         {
-            let data
-            
-            // Exécution de la bonne requête en fonction des paramètres
-            if (message !== undefined && done !== undefined) 
-            {
-                data = await client.query('UPDATE tickets SET message = $1, done = $2 WHERE id = $3 RETURNING *', [message, done, id]);
-            } 
-            else if (message !== undefined)
-            {
-                data = await client.query('UPDATE tickets SET message = $1 WHERE id = $2 RETURNING *', [message, id]);
-            } 
-            else 
-            {
-                data = await client.query('UPDATE tickets SET done = $1 WHERE id = $2 RETURNING *', [done, id]);
-            }
-    
-            // Vérifiction de l'existence de l'id
-            if (data.rowCount > 0) 
-            {
-                rpcr.status = 200 ;
-                rpcr.message = `Le ticket ${id} a bien été modifier` ;
-                rpcr.data = data.rows[0] ;
-            } 
-            else 
-            {
-                rpcr.status = 404 ;
-                rpcr.message = `Le ticket ${id} n'existe pas` ;
-            }
-        }
-        catch (err) 
+            rpcr.status = 200 ;
+            rpcr.message = `Le ticket ${id} a bien été modifier` ;
+            rpcr.data = data.rows[0] ;
+            rpcr.send() ;
+        } 
+        else 
         {
-            console.log(err.stack)
+            rpcr.status = 404 ;
+            rpcr.message = `Le ticket ${id} n'existe pas` ;
+            rpcr.send() ;
         }
     }
-    console.log(`put | /api/tickets | ${rpcr.info()}`);
-    res = rpcr.send(res)
+    catch (err) 
+    {
+        console.log(err.stack)
+        rpcr.send() ;
+    }
 })
 
 ticketsRouter.delete('/:id', async (req, res) => 
 {
-    let rpcr = new Responcer()
+    let rpcr = new Responcer(req, res) ;
     const id = req.params.id;
 
     // Vérifiction du Type de l'id entrante
@@ -202,32 +172,31 @@ ticketsRouter.delete('/:id', async (req, res) =>
     {
         rpcr.status = 400 ;
         rpcr.message = `${id} n'est pas un nombre entier` ;
+        rpcr.send() ;
     }
-    else 
+    try 
     {
-        try 
+        const data = await client.query('DELETE FROM tickets WHERE id = $1', [id]);
+        
+        // Vérifiction de l'existence de l'id
+        if (data.rowCount === 1) 
         {
-            const data = await client.query('DELETE FROM tickets WHERE id = $1', [id]);
-            
-            // Vérifiction de l'existence de l'id
-            if (data.rowCount === 1) 
-            {
-                rpcr.status = 200 ;
-                rpcr.message = `Le ticket ${id} a bien été supprimé` ;
-            } 
-            else 
-            {
-                rpcr.status = 404 ;
-                rpcr.message = `Aucune ticket ne correspond à l'id ${id}` ;
-            }
-        }
-        catch (err) 
+            rpcr.status = 200 ;
+            rpcr.message = `Le ticket ${id} a bien été supprimé` ;
+            rpcr.send() ;
+        } 
+        else 
         {
-            console.log(err.stack);
+            rpcr.status = 404 ;
+            rpcr.message = `Aucune ticket ne correspond à l'id ${id}` ;
+            rpcr.send() ;
         }
     }
-    console.log(`delete | /api/tickets/${id} | ${rpcr.info()}`);
-    res = rpcr.send(res)
+    catch (err) 
+    {
+        console.log(err.stack);
+        rpcr.send() ;
+    }
 })
 
 module.exports = ticketsRouter;
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -4,117 +4,102 @@ const { faillingString } = require('../module/faillingTest') ;
 const client = require('../module/clientData')
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+const accessTokenSecret = 'youraccesstokensecret';
 
 const usersRouter = express.Router();
 
 usersRouter.post('/register',async (req , res) => 
 {
-    let rpcr = new Responcer()
+    let rpcr = new Responcer(req, res) ;
     const pass = req.body.pass
     const user_name = req.body.user_name
     bcrypt.hash(pass, 10, async (err, hash) =>
     {
-        if (!pass || !user_name)
+        if (faillingString(pass) || faillingString(user_name))
         {
             rpcr.status = 400 ;
             rpcr.message = "Structure incorrect : { pass : string , user_name : string }" ;
-        }
-        else if (faillingString(pass))
-        {
-            rpcr.status = 400 ;
-            rpcr.message = `Le mot de pass doit être un texte`
-        }
-        else if (faillingString(user_name))
-        {
-            rpcr.status = 400 ;
-            rpcr.message = `Le nom d'utilisateur doit être un texte`
-        }
-        else 
-        {
-            try
-            {
-                const usersList = await client.query('SELECT id FROM users WHERE username = $1',[user_name]);
-                
-                if (usersList.rowCount > 0) 
-                {
-                    rpcr.status = 400 ;
-                    rpcr.message = `Cet utilisateur existe déjà`
-                }
-                else 
-                {
-                    const data = await client.query('INSERT INTO users (username, password) VALUES ($1,$2) RETURNING id,username', [user_name, hash]);
-                    rpcr.status = 200 ;
-                    rpcr.message = `L'utilisateur ${user_name} à bien été ajouté`
-                    rpcr.data = data.rows[0]
-                }
-            }
-            catch (err)
-            {
-                console.log(err.stack);
-            }
+            rpcr.send() ;
         }
         
-        console.log(`post | /api/users/regiter | ${rpcr.info()}`);
-        res = rpcr.send(res)
+        try
+        {
+            const usersList = await client.query('SELECT id FROM users WHERE username = $1',[user_name]);
+            
+            if (usersList.rowCount > 0) 
+            {
+                rpcr.status = 400 ;
+                rpcr.message = `Cet utilisateur existe déjà`
+                rpcr.send() ;
+            }
+            else 
+            {
+                const data = await client.query('INSERT INTO users (username, password) VALUES ($1,$2) RETURNING id,username', [user_name, hash]);
+                rpcr.status = 200 ;
+                rpcr.message = `L'utilisateur ${user_name} à bien été ajouté`
+                rpcr.data = data.rows[0]
+                rpcr.send() ;
+            }
+        }
+        catch (err)
+        {
+            console.log(err.stack);
+            rpcr.send() ;
+        }
     });
 })
 
-usersRouter.get('/login', async (req, res) => 
+usersRouter.post('/login', async (req, res) => 
 {
-    let rpcr = new Responcer() ;
+    let rpcr = new Responcer(req, res) ;
     const pass = req.body.pass ;
     const user_name = req.body.user_name ;
     
-    if (!pass || !user_name)
+    if (faillingString(pass) || faillingString(user_name))
     {
         rpcr.status = 400 ;
         rpcr.message = "Structure incorrect : { pass : string , user_name : string }" ;
+        rpcr.send() ;
     }
-    else if (faillingString(pass))
+    
+    try 
     {
-        rpcr.status = 400 ;
-        rpcr.message = `Le mot de pass doit être un texte`
-    }
-    else if (faillingString(user_name))
-    {
-        rpcr.status = 400 ;
-        rpcr.message = `Le nom d'utilisateur doit être un texte`
-    }
-    else 
-    {
-        try 
+        const data = await client.query('SELECT password, id FROM users WHERE username = $1', [user_name]);
+        
+        if (data.rowCount === 0)
         {
-            const sqlPass = await (await client.query('SELECT password FROM users WHERE username = $1', [user_name])).rows[0].password;
-            console.log(sqlPass)
-            bcrypt.compare(pass, sqlPass,async (err,result) =>
+            rpcr.status = 404 ;
+            rpcr.message = `Le nom d'utilisateur ou/et le mot de passe sont incorects`;
+            rpcr.send() ;
+        }
+
+        bcrypt.compare(pass, data.rows[0].password,async (err,result) =>
+        {
+            if (result) 
             {
-                console.log(result);
-                if (result) 
-                {
-                    rpcr.status = 200 ;
-                    rpcr.message = `Connection de ${user_name}` ;
-                }
-                else
-                {
-                    rpcr.status = 400 ;
-                    rpcr.message = `Le nom d'utilisateur ou/et le mot de passe sont incorects`
-                }
-                console.log(`get | /api/users/login | ${rpcr.info()}`);
-                res = rpcr.send(res)
-                return ;
-                
-            })
-        }
-        catch (err) 
-        {
-            console.log(err.stack)
-            console.log(`get | /api/users/login | ${rpcr.info()}`);
-            res = rpcr.send(res)
-            return ;
-        }
+                rpcr.status = 200 ;
+                rpcr.message = `Connection de ${user_name}` ;
+                rpcr.data = { 
+                    token : jwt.sign({ id: data.rows[0].id }, accessTokenSecret),
+                    id : data.rows[0].id
+                } ;
+                rpcr.send() ;
+            }
+            else
+            {
+                rpcr.status = 404 ;
+                rpcr.message = `Le nom d'utilisateur ou/et le mot de passe sont incorects` ;
+                rpcr.send() ;
+            }
+        })
     }
-    console.log(`get | /api/users/login | ${rpcr.info()}`);
-    res = rpcr.send(res)
+    catch (err) 
+    {
+        console.log(err.stack) ;
+        rpcr.send() ;
+    }
+    
 })
 
 module.exports = usersRouter;
